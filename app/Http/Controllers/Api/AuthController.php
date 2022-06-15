1ec9a3b7da6ca615642c47c6a1087d03dcb8dc81
pravedens <pravedens@gmail.com>
2022-06-15 19:23:51 +0500

    login complite

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,18 +11,36 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\Client;
 
 class AuthController extends Controller
 {
-    public function login() {
-
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|between:8, 255',
+        ]);
+        if($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+        $passwordGrantClient = Client::where('password_client', 1) -> first();
+        $data = [
+            'grant_type' => 'password',
+            'client_id' => $passwordGrantClient->id,
+            'client_secret' => $passwordGrantClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '*'
+        ];
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+        return app() -> handle($tokenRequest);
     }
 
     public function register(Request $request): Response|JsonResponse|Application|ResponseFactory
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|between:8, 255|confirmed',
         ]);
         if($validator->fails()) {
